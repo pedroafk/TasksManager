be203273800d60ff4dd9f34a46e9c6b11bac20bf
Pedro Figueiredo <pedro.pessoal1996@gmail.com>
2025-07-17 17:31:24 -0300

    Criação do projeto.

---
+++ android/app/src/main/kotlin/com/example/tasks_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tasks_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TasksManager());
+}
+
+class TasksManager extends StatefulWidget {
+  const TasksManager({super.key});
+
+  @override
+  State<TasksManager> createState() => _TasksManagerState();
+}
+
+class _TasksManagerState extends State<TasksManager> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
